41114cb38fe68eaf8990f4852673b694873c0f9a
adamus97 <adampekala97@gmail.com>
2018-05-29 18:07:26 +0200

    Zmiana wyglądu menu głównego, oraz dodanie ikony

--- Fitness/FormStart.Designer.cs
+++ Fitness/FormStart.Designer.cs
@@ -28,23 +28,39 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Menu));
             this.buttonLogin = new System.Windows.Forms.Button();
             this.buttonRegister = new System.Windows.Forms.Button();
             this.buttonExit = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // buttonLogin
             // 
-            this.buttonLogin.Location = new System.Drawing.Point(88, 22);
+            this.buttonLogin.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.buttonLogin.AutoSize = true;
+            this.buttonLogin.FlatAppearance.BorderSize = 2;
+            this.buttonLogin.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonLogin.Font = new System.Drawing.Font("MS Reference Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.buttonLogin.ForeColor = System.Drawing.Color.Brown;
+            this.buttonLogin.Location = new System.Drawing.Point(12, 361);
+            this.buttonLogin.MinimumSize = new System.Drawing.Size(126, 51);
             this.buttonLogin.Name = "buttonLogin";
-            this.buttonLogin.Size = new System.Drawing.Size(126, 50);
+            this.buttonLogin.Size = new System.Drawing.Size(126, 51);
             this.buttonLogin.TabIndex = 0;
             this.buttonLogin.Text = "Logowanie";
             this.buttonLogin.UseVisualStyleBackColor = true;
             // 
             // buttonRegister
             // 
-            this.buttonRegister.Location = new System.Drawing.Point(258, 21);
+            this.buttonRegister.AutoSize = true;
+            this.buttonRegister.FlatAppearance.BorderSize = 2;
+            this.buttonRegister.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonRegister.Font = new System.Drawing.Font("MS Reference Sans Serif", 12F);
+            this.buttonRegister.ForeColor = System.Drawing.Color.Brown;
+            this.buttonRegister.Location = new System.Drawing.Point(174, 360);
+            this.buttonRegister.MinimumSize = new System.Drawing.Size(126, 51);
             this.buttonRegister.Name = "buttonRegister";
             this.buttonRegister.Size = new System.Drawing.Size(126, 51);
             this.buttonRegister.TabIndex = 1;
@@ -53,24 +69,53 @@
             // 
             // buttonExit
             // 
+            this.buttonExit.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.buttonExit.FlatAppearance.BorderSize = 2;
+            this.buttonExit.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonExit.Font = new System.Drawing.Font("MS Reference Sans Serif", 12F);
+            this.buttonExit.ForeColor = System.Drawing.Color.Brown;
             this.buttonExit.Location = new System.Drawing.Point(336, 361);
+            this.buttonExit.MinimumSize = new System.Drawing.Size(126, 51);
             this.buttonExit.Name = "buttonExit";
             this.buttonExit.Size = new System.Drawing.Size(126, 51);
             this.buttonExit.TabIndex = 2;
             this.buttonExit.Text = "Zakończ";
             this.buttonExit.UseVisualStyleBackColor = true;
             // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.InitialImage = ((System.Drawing.Image)(resources.GetObject("pictureBox1.InitialImage")));
+            this.pictureBox1.Location = new System.Drawing.Point(-47, -45);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(566, 442);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
+            // 
             // Menu
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Window;
             this.ClientSize = new System.Drawing.Size(474, 424);
+            this.ControlBox = false;
             this.Controls.Add(this.buttonExit);
             this.Controls.Add(this.buttonRegister);
             this.Controls.Add(this.buttonLogin);
+            this.Controls.Add(this.pictureBox1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(490, 463);
+            this.MinimumSize = new System.Drawing.Size(490, 463);
             this.Name = "Menu";
-            this.Text = "Menu";
+            this.Text = "Menu główne";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -79,6 +124,7 @@
         private System.Windows.Forms.Button buttonLogin;
         private System.Windows.Forms.Button buttonRegister;
         private System.Windows.Forms.Button buttonExit;
+        private System.Windows.Forms.PictureBox pictureBox1;
     }
 }
 
